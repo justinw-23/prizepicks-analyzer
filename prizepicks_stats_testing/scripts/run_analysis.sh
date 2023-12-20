78f0fa6103a4d11f27610b64029c342e3c8de20b
justinw-23 <justinwang23@g.ucla.edu>
2023-12-19 23:45:37 -0800

    standardized scripts to run based on PST

--- prizepicks_stats_testing/scripts/run_analysis.sh
+++ prizepicks_stats_testing/scripts/run_analysis.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+export TZ="America/Los_Angeles"
 current_date=$(date +"%Y-%m-%d")
 current_date_snake_case=$(date +"%Y_%m_%d")
 current_month=$(date +"%B")
 current_year=$(date +"%Y")
 month="${current_month} ${current_year}"
 
-process_csv_path="./process_csv_files.py"
-count_hits_path="./count_hits.py"
-box_score_filename="../data/${month}/${current_date}-Data/nba_box_scores_${current_date_snake_case}.csv"
-edge_picks_filename="../data/${month}/${current_date}-Data/nba_edge_picks_${current_date_snake_case}.csv"
-analyzed_filename="../data/${month}/${current_date}-Data/edge_picks_analyzed_${current_date_snake_case}.csv"
-hits_filename="../data/hits.txt"
+box_score_filename="$(realpath ../data/${month}/${current_date}-Data/nba_box_scores_${current_date_snake_case}.csv)"
+edge_picks_filename="$(realpath ../data/${month}/${current_date}-Data/nba_edge_picks_${current_date_snake_case}.csv)"
+analyzed_filename="$(realpath ../data/${month}/${current_date}-Data/edge_picks_analyzed_${current_date_snake_case}.csv)"
+hits_filename="$(realpath ../data/hits.txt)"
 
-python3 "$process_csv_path" "$box_score_filename" "$edge_picks_filename" "$analyzed_filename"
-python3 "$count_hits_path" "$analyzed_filename" "$hits_filename"
+python3 process_csv_files.py "$box_score_filename" "$edge_picks_filename" "$analyzed_filename"
+python3 count_hits.py "$analyzed_filename" "$hits_filename"
--- prizepicks_stats_testing/scripts/run_edge_spider.sh
+++ prizepicks_stats_testing/scripts/run_edge_spider.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+export TZ="America/Los_Angeles"
 current_date=$(date +"%Y-%m-%d")
 current_date_snake_case=$(date +"%Y_%m_%d")
 current_month=$(date +"%B")
 current_year=$(date +"%Y")
 month="${current_month} ${current_year}"
 
-output_path="../data/${month}/${current_date}-Data/nba_edge_picks_${current_date_snake_case}.csv"
+output_path="$(realpath ../data/${month}/${current_date}-Data/nba_edge_picks_${current_date_snake_case}.csv)"
+venv_path="$(realpath ../../venv/bin/activate)"
 
-source ../../venv/bin/activate
+source "$venv_path"
 
 scrapy crawl edge_picks_spider -o "$output_path"
 

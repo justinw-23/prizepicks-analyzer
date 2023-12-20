7d75e8c6bf99f12a828ce295e814f6c560c620f7
justinw-23 <justinwang23@g.ucla.edu>
2023-12-19 22:09:10 -0800

    made scripts write to directories separated by month, e.g. 'December 2023'

--- prizepicks_stats_testing/scripts/run_analysis.sh
+++ prizepicks_stats_testing/scripts/run_analysis.sh
@@ -2,11 +2,15 @@
 
 current_date=$(date +"%Y-%m-%d")
 current_date_snake_case=$(date +"%Y_%m_%d")
+current_month=$(date +"%B")
+current_year=$(date +"%Y")
+month="${current_month} ${current_year}"
+
 process_csv_path="./process_csv_files.py"
 count_hits_path="./count_hits.py"
-box_score_filename="../data/${current_date}-data/nba_box_scores_${current_date_snake_case}.csv"
-edge_picks_filename="../data/${current_date}-data/nba_edge_picks_${current_date_snake_case}.csv"
-analyzed_filename="../data/${current_date}-data/edge_picks_analyzed_${current_date_snake_case}.csv"
+box_score_filename="../data/${month}/${current_date}-Data/nba_box_scores_${current_date_snake_case}.csv"
+edge_picks_filename="../data/${month}/${current_date}-Data/nba_edge_picks_${current_date_snake_case}.csv"
+analyzed_filename="../data/${month}/${current_date}-Data/edge_picks_analyzed_${current_date_snake_case}.csv"
 hits_filename="..data/hits.txt"
 
 python3 "$process_csv_path" "$box_score_filename" "$edge_picks_filename" "$analyzed_filename"
--- prizepicks_stats_testing/scripts/run_box_scores_spider.sh
+++ prizepicks_stats_testing/scripts/run_box_scores_spider.sh
@@ -2,7 +2,11 @@
 
 current_date=$(date +"%Y-%m-%d")
 current_date_snake_case=$(date +"%Y_%m_%d")
-output_path="../data/${current_date}-data/nba_box_scores_${current_date_snake_case}.csv"
+current_month=$(date +"%B")
+current_year=$(date +"%Y")
+month="${current_month} ${current_year}"
+
+output_path="../data/${month}/${current_date}-Data/nba_box_scores_${current_date_snake_case}.csv"
 
 source ../../venv/bin/activate
 

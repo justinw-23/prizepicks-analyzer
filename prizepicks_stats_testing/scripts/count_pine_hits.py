f8d86f5cfc12bc6ef010a65e52e1beed04ad802f
justinw-23 <justinwang23@g.ucla.edu>
2023-12-23 12:52:30 -0800

    added pine picks analyzer scripts, needs to be finished

---
+++ prizepicks_stats_testing/scripts/count_pine_hits.py
@@ -0,0 +1,26 @@
+import csv
+import sys
+from datetime import datetime
+
+def main():
+    formatted_date = datetime.now().strftime("%m/%d/%y")
+    total = 0
+    hits = 0
+    fpath = sys.argv[1]
+    with open(fpath, 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[10] == "Hit":
+                hits += 1
+            total += 1
+    
+    outpath = sys.argv[2]
+    with open(outpath, "a") as outfile:
+        outfile.write(f"Pine Picks Hits/Total for {formatted_date}: {hits}/{total}\n")
+        # outfile.write(f"\tHits/Total for {formatted_date} over 60.0%: {hits_over_60}/{total_over_60}\n")
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(2)

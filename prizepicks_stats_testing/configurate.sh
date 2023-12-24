b1b53c7bae4c5dfad310a5817c973f9742f8f2ce
justinw-23 <justinwang23@g.ucla.edu>
2023-12-23 23:02:00 -0800

    edited crontab to run ftn picks at 12:00 instead of 12:59 PST

--- prizepicks_stats_testing/configurate.sh
+++ prizepicks_stats_testing/configurate.sh
@@ -7,7 +7,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 patterns=("run_ftn_picks_spider.sh" 
           "run_box_scores_spider.sh"
           "run_analysis.sh")
-new_commands=("59 12 * * * /bin/bash $script_dir/scripts/run_ftn_picks_spider.sh >> /home/ubuntu/crontest.log 2>&1"
+new_commands=("0 12 * * * /bin/bash $script_dir/scripts/run_ftn_picks_spider.sh >> /home/ubuntu/crontest.log 2>&1"
               "45 22 * * * /bin/bash $script_dir/scripts/run_box_scores_spider.sh >> /home/ubuntu/crontest.log 2>&1"
               "47 22 * * * /bin/bash $script_dir/scripts/run_analysis.sh >> /home/ubuntu/crontest.log 2>&1")
 

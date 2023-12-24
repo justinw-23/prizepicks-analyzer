d52f9cdb43100f1dbbaab732b27a0e9ebbaf4960
justinw-23 <justinwang23@g.ucla.edu>
2023-12-24 00:18:36 -0800

    auto committing data for 12-24-2023

--- prizepicks_stats_testing/scripts/auto_commit.sh
+++ prizepicks_stats_testing/scripts/auto_commit.sh
@@ -1,5 +1,6 @@
 current_date=$(date +"%m-%d-%Y")
 "$(cd "$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")")"
+pwd
 
 git add .
 git commit -m "auto committing data for $current_date"
